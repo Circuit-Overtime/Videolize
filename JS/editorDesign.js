942bf123504dc7520a1c2545dc95aabf34a54d35
Circuit-Overtime <ayushbhatt633@gmail.com>
2024-02-04 10:15:40 +0530

    edited the second patch

--- JS/editorDesign.js
+++ JS/editorDesign.js
@@ -1,3 +1,18 @@
+function typeWriterEffectHTML(idOfTextHolder, textToType, speed) {
+  var i = 0;
+  var speed = speed || 55; // Default speed if not provided
+  document.getElementById(idOfTextHolder).innerHTML = "";
+  function type() {
+      if (i < textToType.length) {
+          document.getElementById(idOfTextHolder).innerHTML += textToType.charAt(i);
+          i++;
+          setTimeout(type, speed);
+      }
+  }
+  type(); // Call the function to start the typing effect
+  
+}
+
 document.getElementById("linkedinRedirect").addEventListener("click", () => {
     window.open("https://www.linkedin.com/in/solankighoshvideoeditor/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", "_blank")
   })
@@ -25,3 +40,5 @@ document.getElementById("linkedinRedirect").addEventListener("click", () => {
     window.onload = e => {
         document.getElementById("LazyLoadprofileLogoCont").style.opacity = 0;
     }
+
+    typeWriterEffectHTML("classDesc","With almost 2 years of video editing mastery, I've melded pixels into mesmerizing tales. My expertise spans dynamic cuts, cinematic color grading, and seamless soundscapes, ensuring each video is a captivating visual journey.", 32)
