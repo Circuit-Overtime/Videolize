b72431c62930ccc7d0cb9d6fd7015bc7e0bebdc9
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-07-03 21:29:19 +0530

    Added the code to allow projects

--- JS/autoEditaSocialMediaGallery.js
+++ JS/autoEditaSocialMediaGallery.js
@@ -1,37 +1,17 @@
-const gallery = document.getElementById("SocialMediaImageGallery");
-const SocialGalleryContainer = document.getElementById("FirstRowFirstLayoutEdita");
-SocialGalleryContainer.onmousemove = e => {
-  const mouseX = e.clientX-50,
-        mouseY = e.clientY-300;
-  
-  const xDecimal = mouseX / SocialGalleryContainer.clientWidth,
-        yDecimal = mouseY / SocialGalleryContainer.clientHeight;
-  
-  const maxX = gallery.offsetWidth - SocialGalleryContainer.clientWidth-350,
-        maxY = gallery.offsetHeight - SocialGalleryContainer.clientHeight-350;
-  
-  const panX = maxX * xDecimal * -1,
-        panY = maxY * yDecimal * -1;
-  
-  gallery.animate({
-    transform: `translate(${panX/20}px, ${panY/20}px)`
-  }, {
-    duration: 4000,
-    fill: "forwards",
-    easing: "ease"
-  })
-}
-SocialGalleryContainer.onmouseout = e => {
-    gallery.animate({
-        transform: `translate(${0}px, ${0}px)`
-      }, {
-        duration: 4000,
-        fill: "forwards",
-        easing: "ease"
-      })
-}
-
-
-$('.FirstRowFirstLayoutEdita').tilt({
-    scale: 0.98
-})
+document.querySelectorAll("#image-container > div").forEach(div => {
+  const lazyImg = div.querySelector(".autoEditaLazyLoading");
+  const fullImg = div.querySelector('img[loading="lazy"]');
+  if (lazyImg && fullImg) {
+    // Hide full image initially and add fade-in class
+    fullImg.style.display = "none";
+    fullImg.classList.add("autoEditaFadeIn");
+    fullImg.addEventListener("load", () => {
+      lazyImg.style.display = "none";
+      fullImg.style.display = "";
+      // Trigger fade-in
+      setTimeout(() => {
+        fullImg.classList.add("visible");
+      }, 10);
+    });
+  }
+});
